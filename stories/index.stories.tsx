9cacc8822af4adc4e284cc45797da43da88d3a74
RaoHai <surgesoft@gmail.com>
2018-11-02 12:08:02 +0800

    add storybook

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { generateLayout, generateDOM } from '../test/testUtils';
+import Layout from '../src/components/Layout';
 
+const layout = generateLayout();
 storiesOf("TypeScript and Storybook", module)
-  .add('Sample Widget', () => <div> hello world </div>);
+  .add('Sample Widget', () => <Layout
+    layout={layout}
+    width={1024}
+    grid={[8, 8]}
+  >
+    {generateDOM(layout)}
+  </Layout>);
