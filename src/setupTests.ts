f38680743504dbbb0796830577585c8f6b57c264
RaoHai <surgesoft@gmail.com>
2018-11-22 09:44:07 +0800

    test: fix

--- src/setupTests.ts
+++
@@ -1,6 +0,0 @@
-import { configure } from 'enzyme';
-import EnzymeAdapter from 'enzyme-adapter-react-16';
-import {createSerializer} from 'enzyme-to-json';
-
-configure({ adapter: new EnzymeAdapter() });
-expect.addSnapshotSerializer(createSerializer({ mode: 'deep' }));
